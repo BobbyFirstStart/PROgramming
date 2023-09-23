1f00d11e479e786a8f939cbfb88d38b2b428e38a
Anatoly Starostin <anatoli.starostin@gmail.com>
2023-09-23 14:10:48 +0300

    function for showing images

--- ml_tools.py
+++ ml_tools.py
@@ -1,1 +1,11 @@
+import skimage
+import matplotlib.pyplot as plt
+from skimage import io
+
 # here we plan to develop cool tools for ml
+def show_image(path):
+    img = io.imread(path)
+    plt.figure()
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
