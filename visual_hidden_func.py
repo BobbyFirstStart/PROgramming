8fb6dfbd13ca5d41b4e9b13efbd62dea56c7574c
Evgen-ia <yampolskaya.evgenie@yandex.ru>
2023-11-01 15:45:23 +0200

    first push

---
+++ visual_hidden_func.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path = "data_to_decrypt.bin"
+file = open(file_path, "rb")
+loaded_data = pickle.load(file)
+file.close()
+
+X = loaded_data['x_train']
+Y = loaded_data['y_train']
+
+
+degree = 20
+
+# Выполняем МНК для полиномиальной регрессии
+coefficients = np.polyfit(X, Y, degree)
+
+# Создаем полиномиальную модель на основе коэффициентов
+polynomial_model = np.poly1d(coefficients)
+
+# Генерируем значения для графика
+x_values = np.linspace(min(X), max(X), 100)
+y_values = polynomial_model(x_values)
+
+# Выводим результаты
+plt.scatter(X, Y, label='Данные')
+plt.plot(x_values, y_values, label='Полиномиальная регрессия', color='red')
+plt.legend()
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
